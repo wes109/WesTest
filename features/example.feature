d51dbde268a52742e58a688b1f3f99e6b4db24ee
wes109 <wwchome1@gmail.com>
2020-05-08 09:32:31 -0500

    First commit

--- features/example.feature
+++ features/example.feature
@@ -1,9 +1,114 @@
-Feature: Example feature
-  As a user of Cucumber.js
-  I want to have documentation on Cucumber
-  So that I can concentrate on building awesome applications
+Feature: BusPatrol Test
 
-  Scenario: Reading documentation
-    Given I am on the Cucumber.js GitHub repository
-    When I click on "CLI"
-    Then I should see "Running specific features"
+  Scenario: W3Schools 1
+    Given I am on "https://www.w3schools.com/js/js_json_intro.asp"
+    Then I should see "JSON: "
+    Then I should see "J"
+    Then I should see "ava"
+    Then I should see "S"
+    Then I should see "cript "
+    Then I should see "O"
+    Then I should see "bject"
+    Then I should see "N"
+    Then I should see "otation."
+    Then I should see "JSON is a syntax for storing and exchanging data."
+    Then I should see "JSON is text, written with JavaScript object notation."
+    Then I find 3 buttons with text "Try it Yourself"
+    Then I click link "Next ❯"
+    Then I verify I am on page "https://www.w3schools.com/js/js_json_syntax.asp"
+
+  Scenario: W3Schools 2
+    Given I am on "https://www.w3schools.com/js/js_json_syntax.asp"
+    Then I should see "JSON Syntax Rules"
+    Then I should see list item "Data is in name/value pairs"
+    Then I should see list item "Data is separated by commas"
+    Then I should see list item "Curly braces hold objects"
+    Then I should see list item "Square brackets hold arrays"
+    Then I should see " must be one of the following data types:"
+    Then I should see list item "a string"
+    Then I should see list item "a number"
+    Then I should see list item "an object (JSON object)"
+    Then I should see list item "an array"
+    Then I should see list item "a boolean"
+    Then I should see list item "null"
+    Then I should see h2 "JSON Files"
+    Then I should see list item "The file type for JSON files is "
+    Then I should see list item "The MIME type for JSON text is"
+    Then I find 4 buttons with text "Try it Yourself"
+    Then I click last link "Next"
+    Then I verify I am on page "https://www.w3schools.com/js/js_json_xml.asp"
+
+  Scenario: W3Schools 3
+    Given I am on "https://www.w3schools.com/js/js_json_xml.asp"
+    Then I should see "JSON vs XML"
+    Then I should see "JSON HTML" link in sidebar
+    #Then I click "JSON HTML" link in sidebar
+    #Then I verify I am on page "https://www.w3schools.com/js/js_json_html.asp"
+    
+  Scenario: W3Schools 4
+    Given I am on "https://www.w3schools.com/"
+    Then I should see "Learn HTML"
+    Then I should see "Learn HTML" link in sidebar
+    # Then I click "Learn HTML" link in sidebar
+    # Then I verify I am on page "https://www.w3schools.com/html/default.asp"
+    # Below is my replacement step to just get to the page so I can continue Test
+    Given I am on "https://www.w3schools.com/html/default.asp"
+    #Then I should see it in the rgb div
+    Then I fill "//input[@name='ex1']" field with "title"
+
+  Scenario: AlertBus
+    Given I am on "https://www.alertbus.com"
+    Then I should see h1 "School Bus Safety Program"
+    Then I should see "Find A Citation"
+    Then I fill "//input[@id='citation_number']" field with "garbage"
+    Then I fill "//input[@id='license_plate']" field with "trash"
+    #Then I click on link with name "commit"
+    #Then I verify "citation_number" field is empty
+    #Then I verify "license_plate" field is empty
+    #Then I should see rgb text "Please enter the entire citation number, beginning with the state code (ex: 'TX-01-...')"
+  Scenario: MarkSheet
+    Given I am on "https://marksheet.io/html-forms.html"
+    Then I should see "Example: a complete signup form"
+    Then I click radio "miss"
+    Then I fill "//p[2]//input[1]" field with "John"
+    Then I fill "//p[3]//input[1]" field with "Smith"
+    Then I fill "//p[4]//input[1]" field with "john.smith@test.com"
+    Then I fill "//p[5]//input[1]" field with "555-5555"
+    Then I fill "//p[6]//input[1]" field with "password"
+    Then I fill "//p[7]//input[1]" field with "password"
+    Then I select "United States" option from drop down
+    Then I select checkbox "//form//label[1]//input[1]"
+    Then I click button "Sign up"
+    Then I verify I am on page "https://marksheet.io/signup"
+  Scenario: Cupcake Ipsum
+    Given I am on "http://www.cupcakeipsum.com"
+    Then I should see "Cupcake Ipsum - Sugar-coated Lorem Ipsum Generator"
+    # Then I check value of "number_of_paragraphs" text box
+    Then I click radio "short"
+    Then I click radio "startsWithCupcakeIpsum"
+    Then I find 1 "Cupcake ipsum dolor sit amet"
+    Then I find 0 "Copy to Clipboard"
+    Then I click button "Generate"
+    Then I find 2 "Cupcake ipsum dolor sit amet"
+
+  Scenario: BoardGameGeek
+    Given I am on "https://boardgamegeek.com/advsearch/boardgame"
+    Then I fill "//input[@id='advsearch-title']" field with "Harry Potter and the Sorcerer's Stone Trivia Game"
+    Then I fill "//input[@id='advsearch-yearpublished-min']" field with "1999"
+    Then I fill "//input[@id='advsearch-yearpublished-max']" field with "2000"
+    Then I select "15 minutes" option from "advsearch-min-playing-time" drop down
+    Then I select "1.5 hours" option from "advsearch-max-playing-time" drop down
+    Then I find 0 "Novel-based" on boardgamegeek
+    Then I click link "Filter on Board Game Category »"
+    #Then I find 1 "Novel-based" on boardgamegeek ---- Does not work due to HTML not changing
+    Then I click on button with name "B1"
+    Then I verify I am on page "https://boardgamegeek.com/geeksearch.php?action=search&advsearch=1&objecttype=boardgame&q=Harry%20Potter%20and%20the%20Sorcerer%27s%20Stone%20Trivia%20Game&include%5Bdesignerid%5D=&geekitemname=&geekitemname=&include%5Bpublisherid%5D=&range%5Byearpublished%5D%5Bmin%5D=1999&range%5Byearpublished%5D%5Bmax%5D=2000&range%5Bminage%5D%5Bmax%5D=&floatrange%5Bavgrating%5D%5Bmin%5D=&floatrange%5Bavgrating%5D%5Bmax%5D=&range%5Bnumvoters%5D%5Bmin%5D=&floatrange%5Bavgweight%5D%5Bmin%5D=&floatrange%5Bavgweight%5D%5Bmax%5D=&range%5Bnumweights%5D%5Bmin%5D=&colfiltertype=&searchuser=&range%5Bminplayers%5D%5Bmax%5D=&range%5Bmaxplayers%5D%5Bmin%5D=&playerrangetype=normal&range%5Bleastplaytime%5D%5Bmin%5D=15&range%5Bplaytime%5D%5Bmax%5D=90&B1=Submit"
+    Then I should see "Harry Potter and the Sorcerer"
+
+  Scenario: gluons
+    Given I am on "https://gluons.github.io/vue-radio-toggle-buttons"
+    Then I click radio "Value 3" on github
+    # Selecting by value=3 results in Value 4 radio being selected, yet putting 5 or 2 works
+    Then I should see "Value 3" on github
+    Then I click radio "Value 5" on github
+    Then I should see "Value 5" on github
--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -1,20 +1,203 @@
 var seleniumWebdriver = require('selenium-webdriver');
 var {defineSupportCode} = require('cucumber');
+var {setDefaultTimeout} = require('cucumber');
+var assert = require('assert');
+var promise = require('selenium-webdriver').promise;
+
+// Default timeout set here to prevent page load errors. 5000ms for element locators hardcoded below.
+setDefaultTimeout(30000);
+
+// This function just allows me to slow down the script. Otherwise, it zooms through.
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}   
 
 defineSupportCode(function({Given, When, Then}) {
-  Given('I am on the Cucumber.js GitHub repository', function() {
-    return this.driver.get('https://github.com/cucumber/cucumber-js/tree/master');
+  Given('I am on {stringInDoubleQuotes}', function(link) {
+    return this.driver.get(link);
+
   });
 
-  When('I click on {stringInDoubleQuotes}', function (text) {
+  Then('I click on {stringInDoubleQuotes}', function (text) {
     return this.driver.findElement({linkText: text}).then(function(element) {
       return element.click();
     });
   });
 
+  Then('I click on button with name {stringInDoubleQuotes}', function (text) {
+    var xpath = "//input[@name='" + text + "']"
+    return this.driver.findElement({xpath:xpath}).then(function(element) {
+      return element.click();
+    });
+  });
   Then('I should see {stringInDoubleQuotes}', function (text) {
     var xpath = "//*[contains(text(),'" + text + "')]";
     var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
     return this.driver.wait(condition, 5000);
+  });
+  Then('I should see {stringInDoubleQuotes} on github', function (text) {
+    var xpath = "//div[contains(@class, 'result') and text() = '" + text + "']";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I should see h2 {stringInDoubleQuotes}', function (text) {
+    var xpath = "//h2[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I should see xpath {stringInDoubleQuotes}', function (text) {
+    var xpath = text;
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I should see list item {stringInDoubleQuotes}', function (text) {
+    var xpath = "//li[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I find {int} buttons with text {stringInDoubleQuotes}', async function (int, text) {
+    var count = 0;
+    var xpath = "//a[contains(text(),'" + text + "')]";
+    let links = await this.driver.findElements({xpath:xpath});
+    for(let link of links) {
+        count = count + 1
+        text = await link.getText();
+    }
+    assert(count == int)
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I click link {stringInDoubleQuotes}', async function (text) {
+    var xpath = ".//a[contains(text(),'" + text + "')]";
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(2000)
+  });
+  Then('I click button {stringInDoubleQuotes}', async function (text) {
+    var xpath = "//button[contains(text(),'" + text + "')]";
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(2000)
+  });
+  Then('I click last link {stringInDoubleQuotes}', async function (text) {
+    var xpath = "(.//a[contains(text(),'" + text + "')])[last()]";
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(500)
+  });
+  Then('I should see {stringInDoubleQuotes} link in sidebar', async function (text) {
+    var xpath = "//a[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    await sleep(1000)
+    return this.driver.wait(condition, 5000);
+  });
+
+// ElementNotVisibleError: element not interactable
+// This would require website code changes to test as I have tried exact Xpath (to ensure I clicked the exact element) and it did not work
+  Then('I click {stringInDoubleQuotes} link in sidebar', async function (text) {
+    var xpath = "//a[contains(text(),'" + text + "')]";
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(1000)
+  });
+
+  Then('I should see it in the rgb div', async function () {
+    var xpath = "/div[contains(@style,'background-color: rgb(212, 237, 218)')]"
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  // Could not resolve current URL -- Decided to go with direct xpath as it is broad enough to support testing
+  // Original plan was to if/elseif the change between id and name attribute between sites
+  Then('I fill {stringInDoubleQuotes} field with {stringInDoubleQuotes}', async function (xpath, text) {
+    this.driver.findElement({xpath:xpath}).sendKeys(text)
+    await sleep(500)
+  });
+
+  Then('I select {stringInDoubleQuotes} option from drop down', async function (text) {
+    var xpath = "//option[contains(text(),'" + text + "')]"
+    this.driver.findElement({xpath:xpath}).click()
+    await sleep(500)
+  });
+
+  Then('I select checkbox {stringInDoubleQuotes}', async function (xpath) {
+    this.driver.findElement({xpath:xpath}).click()
+    await sleep(500)
+  });
+  Then('I verify {stringInDoubleQuotes} field is empty', async function (name) {
+    // Added sleep here instead of when I click View Citation
+    await sleep(1000)
+    var xpath = "//input[@name='" + name + "']"
+    var field = this.driver.findElement({xpath:xpath})
+    console.log(field.value)
+    // FIX THIS
+  });
+
+  Then('I verify I am on page {stringInDoubleQuotes}', async function (text) {
+    var condition = seleniumWebdriver.until.urlIs(text);
+    await sleep(2000)
+    return this.driver.wait(condition, 10000);
+  });
+
+  Then('I should see h1 {stringInDoubleQuotes}', function (text) {
+    var xpath = "//h1[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+
+  Then('I should see rgb text {stringInDoubleQuotes}', function (text) {
+    var xpath = "//div[contains(@style,'color: rgb(185, 74, 72)')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I click radio {stringInDoubleQuotes}', async function (value) {
+    if(value == 'startsWithCupcakeIpsum') {
+      var xpath = "//input[@name='" + value + "']";
+    }
+    else
+    {
+      var xpath = "//input[@value='" + value + "']";
+    }
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(500)
+  });
+  Then('I should see rgb text {stringInDoubleQuotes}', function (text) {
+    var xpath = "//div[contains(@style,'color: rgb(185, 74, 72)')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+  Then('I click radio {stringInDoubleQuotes} on github', async function (text) {
+    
+    var xpath = "//label[contains(text(),'" + text + "')]";
+    await this.driver.findElement({xpath:xpath}).click();
+    await sleep(1000)
+  });
+
+  Then('I find {int} {stringInDoubleQuotes}', async function (int, text) {
+    await sleep(500)
+    var count = 0;
+    var xpath = "//*[contains(text(),'" + text + "')]";
+    let links = await this.driver.findElements({xpath:xpath});
+    for(let link of links) {
+        count = count + 1
+        text = await link.getText();
+    }
+    console.log("I FOUND: " + count)
+    assert(count == int)
+  });
+
+  // Did not have time to create logic, so I tried using aria-hidden attribute
+  // Unfortunately, no noticeable HTML code change results from expanding "Filter on Board Game Category »"
+  Then('I find {int} {stringInDoubleQuotes} on boardgamegeek', async function (int, text) {
+    var count = 0;
+    var xpath = "//span[@aria-hidden='false' and contains(text(),'" + text + "')]";
+    let links = await this.driver.findElements({xpath:xpath});
+    for(let link of links) {
+        count = count + 1
+        text = await link.getText();
+    }
+    assert(count == int)
+  });
+  Then('I select {stringInDoubleQuotes} option from {stringInDoubleQuotes} drop down', async function (option, id) {
+    var xpath = "//select[@id='" + id + "']//option[contains(text(),'" + option + "')]"
+    this.driver.findElement({xpath:xpath}).click()
+    await sleep(500)
   });
 });
